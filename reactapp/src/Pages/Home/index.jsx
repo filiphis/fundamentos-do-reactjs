8441a4340f7f313abfef188d5dc048073a1cd858
Luiz Felipe da Silveira <l.filiphis@hotmail.com>
2022-07-11 22:28:26 -0300

    :construction: useEffect

--- reactapp/src/Pages/Home/index.jsx
+++ reactapp/src/Pages/Home/index.jsx
@@ -1,10 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles.css'
 import Card from '../../components/Card'
 
 function App() {
   const [userName, setUserName] = useState('');
   const [students, setStudents] = useState([]);
+  const [gitUser, setGitUser] = useState({name: '', avatar: ''})
 
   function handleAddStudent(){
     const student = {
@@ -16,13 +17,33 @@ function App() {
       })
     }
 
-    setStudents([...students, student])
+    setStudents(students => [...students, student])
   }
+
+  useEffect(  () => {
+    async function fetchData() {
+      const response = await fetch('https://api.github.com/users/filiphis');
+      const data = await response.json();
+      setGitUser({
+        name: data.name,
+        avatar: data.avatar_url
+      });
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <div className='container'>
-      <h1>Lista de presença</h1>
-      <input type="text" placeholder='Digite o nome...' onChange={e => setUserName(e.target.value)} />
+      <header>
+        <h1>Lista de presença</h1>
+
+        <div className='perfil__wrapper'>
+          <strong className='perfil__name'>{gitUser.name}</strong>
+          <img className='image__perfil' src={gitUser.avatar} alt="" />
+        </div>
+      </header>
+      <input type="text" placeholder='Digite o nome...' onChange={e => setUserName(e.target.value)} onKeyDown={e => e.key == 'Enter' ? handleAddStudent() : '' } />
       <button type="submit" onClick={handleAddStudent}>Adicionar</button>
 
     
